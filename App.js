c329e9f33f13f3582e90e344a18c0e0c19de4280
Key0817 <keylorpalacios85@gmail.com>
2024-05-24 00:14:23 -0600

    Avance de la practica realizada en clase

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txtBienve}>Bienvenidos!</Text>
+      <Text style={styles.titleAcc}>Ingresar con tu cuenta</Text>
+
+      <TextInput style={styles.txtInput} placeholder='multimedios@gmail.com'></TextInput>
+      <TextInput style={styles.txtInput} placeholder='...125s'></TextInput>
+      <Text>Ingresar con tu cuenta?</Text>
+
+      <Button title='Iniciar Sesión'/>
+      <Text>Ya tienes cuenta? Registrarse</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  txtBienve: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#34434D',
+    textAlign: 'left',
+    paddingLeft: 30,
+  },
+
+  titleAcc: {
+    fontSize: 20,
+    fontWeight: 'light',
+    color: 'gray',
+    textAlign: 'left',
+    paddingLeft: 30,
+  },
+
+  txtInput: {
+    width: '80%',
+    height: 50,
+    borderRadius: 30, 
+    paddingLeft: 30,
+    marginTop: 20,
+    marginLeft: 20,
+    borderColor: 'gray',
+    color: 'gray',
+    backgroundColor: '#F5F5F5',
+    shadowColor: '#000',
+    textShadowOffset: {width:0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    
   },
 });
