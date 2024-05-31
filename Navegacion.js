a98eedb0f9632eb52240f9cb152303ba78e3a0c1
Key0817 <keylorpalacios85@gmail.com>
2024-05-31 16:41:20 -0600

    Pasos previos para luego agregar la navegación

---
+++ Navegacion.js
@@ -0,0 +1,19 @@
+import React from "react"
+import 'react-native-gesture-handler';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; //1. importar tab
+import { NavigationContainer } from "@react-navigation/native";
+
+import Home from "./my-project/Navegacion/Home";
+import Home2 from "./my-project/Navegacion/Home2";
+import HomeBtn from "./my-project/Navegacion/HomeBtn";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const stack = createStackNavigator();
+
+export default function Navegacion() {
+    return(  
+
+
+     );
+}
+
